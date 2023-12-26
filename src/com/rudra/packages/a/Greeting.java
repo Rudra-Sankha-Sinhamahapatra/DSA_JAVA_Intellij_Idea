5d8b3c7eafc8ecca1749ccd526c9a1c0b664a341
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-12-26 22:32:00 +0530

    Initial commit

---
+++ src/com/rudra/packages/a/Greeting.java
@@ -0,0 +1,10 @@
+package com.rudra.packages.a;
+
+
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
---
+++ src/com/rudra/packages/b/Greeting.java
@@ -0,0 +1,11 @@
+package com.rudra.packages.b;
+
+
+import static com.rudra.packages.b.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        message();
+    }
+}
