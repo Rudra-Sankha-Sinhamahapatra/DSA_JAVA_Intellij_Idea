f7c191ab9c5d5a5eb447838ce02b390b530133c6
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-12-27 16:07:34 +0530

    updated

--- src/com/rudra/oops2/Human.java
+++ src/com/rudra/oops2/Human.java
@@ -10,12 +10,19 @@ public class Human {
         boolean married;
         static long population;
 
+         static void message(){
+                 System.out.println("Hello world!");
+//                 System.out.println(this.age);
+             //non static properties cant be used in a static function
+         }
     public Human( int age, String name,int salary, boolean married){
             this.age = age;
             this.name = name;
             this.salary = salary;
             this.married = married;
             Human.population+=1;
+
+//            Human.message();
         }
 
 
