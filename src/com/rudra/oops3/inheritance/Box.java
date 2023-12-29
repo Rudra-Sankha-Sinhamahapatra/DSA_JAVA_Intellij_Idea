5970dffff5e935602b963807591f5697e45ae117
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-12-29 16:36:39 +0530

    updated

---
+++ src/com/rudra/oops3/inheritance/Box.java
@@ -0,0 +1,38 @@
+package com.rudra.oops3.inheritance;
+
+public class Box {
+    double l;
+    double h;
+    double w;
+
+    Box(){
+        this.h=-1;
+        this.l=-1;
+        this.w=-1;
+    }
+
+    //cube
+    Box(double side){
+        super();
+        this.w=side;
+        this.l=side;
+        this.h=side;
+    }
+
+   public Box(double l,double h,double w){
+        this.w=w;
+        this.l=l;
+        this.h=h;
+    }
+
+    Box(Box old){
+        this.l=old.l;
+        this.w=old.w;
+        this.h=old.h;
+    }
+
+    public void information(){
+        System.out.println("Running the box");
+    }
+
+}
---
+++ src/com/rudra/oops3/inheritance/BoxWeight.java
@@ -0,0 +1,16 @@
+package com.rudra.oops3.inheritance;
+
+public class BoxWeight extends Box{
+double weight;
+
+    public BoxWeight() {
+        this.weight = -1;
+    }
+
+    public BoxWeight(double l, double h, double w, double weight) {
+        super(l, h, w);//call the parent class constructor
+        //used to initialize values present in parent class
+        //calling the parent class constructor
+        this.weight = weight;
+    }
+}
