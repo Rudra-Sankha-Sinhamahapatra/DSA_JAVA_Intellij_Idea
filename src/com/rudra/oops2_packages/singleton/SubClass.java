a60aa61bc1f213868efe5663d1d786b0e1778129
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-01-07 17:04:29 +0530

    updated

--- src/com/rudra/oops2_packages/singleton/SubClass.java
+++ src/com/rudra/oops2_packages/singleton/SubClass.java
@@ -8,6 +8,44 @@ public class SubClass extends A{
 
     public static void main(String[] args) {
        SubClass obj=new SubClass(32,"Hello");
+
+//        A obj=new A(32,"Hello");
+// will not work because only the subclass can use protected memebers of base class
+// when it is in different package
+
+        int n= obj.num;
+        System.out.println(n);
+    }
+}
+
+class SubSubclass extends SubClass {
+    public SubSubclass(int num, String name) {
+        super(num, name);
+    }
+    public static void main(String[] args) {
+
+        SubSubclass obj=new SubSubclass(32,"Hello");
+//        SubClass obj=new SubClass(32,"Hello");
+        //will not work because the protected member is in the different package
+        //so we cant access through its parent class in the subclass
+
+        int n= obj.num;
+        System.out.println(n);
+    }
+
+}
+
+class SubClass2 extends A{
+    public SubClass2(int num, String name) {
+        super(num, name);
+    }
+
+    public static void main(String[] args) {
+//        SubClass obj=new SubClass(32,"Hello");
+        //will not work because it has no connection with  SubClass
+
+        SubClass2 obj=new SubClass2(32,"Hello");
+
         int n= obj.num;
         System.out.println(n);
     }
--- src/com/rudra/oops4_access/SubClass.java
+++ src/com/rudra/oops4_access/SubClass.java
@@ -6,7 +6,43 @@ public class SubClass extends A{
     }
 
     public static void main(String[] args) {
+//        SubClass obj=new SubClass(32,"Hello");
+        A obj=new A(32,"Hello");
+        //will work because base class can use the protected member  when it is
+        // in the same package
+
+        int n= obj.num;
+        System.out.println(n);
+    }
+}
+
+class SubSubclass extends SubClass{
+    public SubSubclass(int num, String name) {
+        super(num, name);
+    }
+    public static void main(String[] args) {
+
+//        SubSubclass obj=new SubSubclass(32,"Hello");
         SubClass obj=new SubClass(32,"Hello");
+
+        //will  work because the protected member is in the same package
+        //so, we can access through its parent class in the subclass
+
+        int n= obj.num;
+        System.out.println(n);
+    }
+
+}
+class SubClass2 extends A{
+    public SubClass2(int num, String name) {
+        super(num, name);
+    }
+
+    public static void main(String[] args) {
+
+//        A obj=new A(32,"Hello");
+        SubClass2 obj=new SubClass2(32,"Hello");
+
         int n= obj.num;
         System.out.println(n);
     }
