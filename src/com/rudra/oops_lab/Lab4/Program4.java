803312de98844936da271f5cb07f78f73ae1d4c1
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-03-05 22:51:50 +0530

    updated

--- src/com/rudra/oops_lab/Lab4/Program4.java
+++ src/com/rudra/oops_lab/Lab4/Program4.java
@@ -1,5 +1,6 @@
 /*
-4.Aim of the program :Write a class Account containing acc_no, balance as data members and two methods as input() for taking input from user and disp() method to display the details.
+4.Aim of the program :Write a class Account containing acc_no, balance as data members and two methods as
+input() for taking input from user and disp() method to display the details.
 Create a subclass Person which has name and aadhar_no as extra data members and override disp() function.
 Write the complete progrm to take and print details of three persons.
 
