bab0e77bf62c94539c2436c5908a1506dfa4d06c
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-05-16 04:36:43 +0530

    updated

--- src/com/rudra/DSA/Arrays/Easy/Interview_Questions.java
+++ src/com/rudra/DSA/Arrays/Easy/Interview_Questions.java
@@ -149,5 +149,15 @@ public int removeDuplicates(int[] nums) {
             }
         }
 
+        //https://www.geeksforgeeks.org/problems/who-will-win-1587115621/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=who-will-win
+        static int searchInSorted(int arr[], int n, int k){
+            for(int i=0;i<n;i++){
+                if(arr[i]==k){
+                    return 1;
+                }
+            }
+            return -1;
+        }
+
     }
 
