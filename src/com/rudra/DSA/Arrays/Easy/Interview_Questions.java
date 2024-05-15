936b103c3ce67cd3faa5abe06f87636e25512cd4
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-05-16 03:40:41 +0530

    updated

--- src/com/rudra/DSA/Arrays/Easy/Interview_Questions.java
+++ src/com/rudra/DSA/Arrays/Easy/Interview_Questions.java
@@ -135,4 +135,19 @@ public int removeDuplicates(int[] nums) {
             end--;
         }
     }
-}
+
+        //https://leetcode.com/problems/move-zeroes/description/
+        public void moveZeroes(int[] arr) {
+            int j=0;
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]!=0){
+                    arr[j++]=arr[i];
+                }
+            }
+            for(int i=j;i<arr.length;i++){
+                arr[i]=0;
+            }
+        }
+
+    }
+
