2c5a03b39fdf5cf24c91a3620aa38aaf5ef6a7a7
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-11 17:01:18 +0530

    updated

--- src/com/rudra/DSA/Arrays/Medium/InterviewQuestions.java
+++ src/com/rudra/DSA/Arrays/Medium/InterviewQuestions.java
@@ -241,6 +241,25 @@ public int majorityElement(int[] arr) {
             return nums;
         }
 
+ //optimal
+ public int[] rearrangeArray2(int[] arr) {
+     int n=arr.length;
+     int ans[]=new int[n];
+
+     int posIndex=0,negIndex=1;
+     for(int i=0;i<n;i++){
+         if(arr[i]<0){
+             ans[negIndex]=arr[i];
+             negIndex+=2;
+         }
+         else{
+             ans[posIndex]=arr[i];
+             posIndex+=2;
+         }
+     }
+     return ans;
+ }
+
 
 }
 
