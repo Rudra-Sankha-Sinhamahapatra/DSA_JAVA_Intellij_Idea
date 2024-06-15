f772ddba2731ee4d357c04c0c1e992a869366c45
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-15 20:25:22 +0530

    updated

--- src/com/rudra/DSA/Stacks_and_Queues/Stacks_Queue/Easy/Interview_Questions.java
+++ src/com/rudra/DSA/Stacks_and_Queues/Stacks_Queue/Easy/Interview_Questions.java
@@ -99,4 +99,58 @@ public class Interview_Questions {
         }
     }
 
+
+    class Q5 {
+
+        Stack <Integer> input =new Stack<>();
+        Stack <Integer> output=new Stack<>();
+        public Q5() {
+
+        }
+
+        public void push(int x) {
+            while(input.empty()==false){
+                output.push(input.peek());
+                input.pop();
+            }
+            input.push(x);
+            while(output.empty()==false){
+                input.push(output.peek());
+                output.pop();
+            }
+        }
+
+        public int pop() {
+            if(input.empty()){
+                return -1;
+            }
+            int val=input.peek();
+            input.pop();
+            return val;
+        }
+
+        public int peek() {
+            if(input.empty()){
+                return -1;
+            }
+            return input.peek();
+        }
+
+
+        public boolean empty() {
+            return input.isEmpty();
+        }
+    }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+
+
+
 }
