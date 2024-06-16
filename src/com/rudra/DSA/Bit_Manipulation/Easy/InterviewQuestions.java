773458f59d7ccf82de3c1de42f8cfa00a3eec524
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-16 22:20:07 +0530

    updated

--- src/com/rudra/DSA/Bit_Manipulation/Easy/InterviewQuestions.java
+++ src/com/rudra/DSA/Bit_Manipulation/Easy/InterviewQuestions.java
@@ -3,6 +3,7 @@ package com.rudra.DSA.Bit_Manipulation.Easy;
 public class InterviewQuestions {
 
     //https://leetcode.com/problems/power-of-two/description/
+    //Brute
     class Q1 {
         public boolean isPowerOfTwo(int n) {
             boolean value=false;
@@ -26,5 +27,20 @@ public class InterviewQuestions {
         }
     }
 
+//Optimal
+class Q1_1 {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        int power = 1;
+        while (power < n) {
+            power *= 2;
+        }
+
+        return power == n;
+    }
+}
 
 }
