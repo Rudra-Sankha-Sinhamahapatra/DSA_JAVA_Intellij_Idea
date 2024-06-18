00bfb91f1aed88291b56e223348a2a8be7d0700f
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-18 21:56:09 +0530

    Leetcode 50 Added

--- src/com/rudra/DSA/Recursion/Easy/InterviewQuestions.java
+++ src/com/rudra/DSA/Recursion/Easy/InterviewQuestions.java
@@ -30,4 +30,26 @@ public class InterviewQuestions {
             return ans*sign;
         }
     }
+
+
+    //https://leetcode.com/problems/powx-n/description/
+    class Q2{
+        public double myPow(double x, int n) {
+            double ans=1.0;
+            long nn=n;
+            if(nn<0) nn=-1*nn;
+            while(nn>0){
+                if(nn%2==1){
+                    ans=ans*x;
+                    nn=nn-1;
+                }
+                else{
+                    x=x*x;
+                    nn=nn/2;
+                }
+            }
+            if(n<0) ans=(double)(1.0)/(double)(ans);
+            return ans;
+        }
+    }
 }
