662a4709712534c99faf456fdfdb33dfbfd42d0d
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-20 00:05:45 +0530

    Added leetcode 74

--- src/com/rudra/DSA/Arrays/Medium/InterviewQuestions.java
+++ src/com/rudra/DSA/Arrays/Medium/InterviewQuestions.java
@@ -382,4 +382,24 @@ public int longestConsecutive2(int[] a) {
     return longest;
 }
 
+//https://leetcode.com/problems/search-a-2d-matrix/
+    //optimal
+        public boolean searchMatrix(int[][] matrix, int target) {
+            int n=matrix.length;
+            int m=matrix[0].length;
+
+            int low=0,high=n*m-1;
+            while(low<=high){
+                int mid=(low+high)/2;
+                int row=mid/m,col=mid%m;
+                if(matrix[row][col]==target){
+                    return true;
+                }
+                else if(matrix[row][col]<target){
+                    low=mid+1;
+                }
+                else high=mid-1;
+            }
+            return false;
+        }
 }
