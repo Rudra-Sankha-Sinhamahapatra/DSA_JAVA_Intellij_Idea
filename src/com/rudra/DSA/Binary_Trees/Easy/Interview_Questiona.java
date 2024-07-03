184973657b69ef22d4c56a4dc590738ddf122c65
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-07-03 23:45:32 +0530

    Added Leetcode 144

--- src/com/rudra/DSA/Binary_Trees/Easy/Interview_Questiona.java
+++ src/com/rudra/DSA/Binary_Trees/Easy/Interview_Questiona.java
@@ -1,6 +1,28 @@
 package com.rudra.DSA.Binary_Trees.Easy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Interview_Questiona {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     //https://www.geeksforgeeks.org/problems/introduction-to-trees/1
     static int countNodes(int i) {
         int ans=1;
@@ -15,5 +37,22 @@ public class Interview_Questiona {
         return ans;
     }
 
+
+    //https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer>answer=new ArrayList<>();
+        preOrder(root,answer);
+        return answer;
+    }
+
+    private void preOrder(TreeNode node,List<Integer>answer){
+        if(node==null){
+            return;
+        }
+        answer.add(node.val);
+        preOrder(node.left,answer);
+        preOrder(node.right,answer);
+    }
 
 }
