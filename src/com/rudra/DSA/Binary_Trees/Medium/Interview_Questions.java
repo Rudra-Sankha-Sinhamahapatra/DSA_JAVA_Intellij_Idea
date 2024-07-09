4e8d79e38f1bb3b400e38026434214aa19ab3a18
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-07-09 09:30:45 +0530

    Leetcode 104 LevelWise Approach Added

--- src/com/rudra/DSA/Binary_Trees/Medium/Interview_Questions.java
+++ src/com/rudra/DSA/Binary_Trees/Medium/Interview_Questions.java
@@ -1,5 +1,7 @@
 package com.rudra.DSA.Binary_Trees.Medium;
 
+import java.util.*;
+
 public class Interview_Questions {
 
     public class TreeNode {
@@ -22,12 +24,30 @@ public class Interview_Questions {
 
     // https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
     //Recursive approach- best approach
-    public int maxDepth(TreeNode root) {
+    public int maxDepthRecursive(TreeNode root) {
         if(root==null){
             return 0;
         }
-        int lh=maxDepth(root.left);
-        int rh=maxDepth(root.right);
+        int lh=maxDepthRecursive(root.left);
+        int rh=maxDepthRecursive(root.right);
         return 1+Math.max(lh,rh);
+    }
+
+    //Levelwise Approach
+    public int maxDepthLevelWise(TreeNode root) {
+        int depth=0;
+        if(root==null) return depth;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            depth++;
+            for(int i=0;i<size;i++){
+                TreeNode temp=q.poll();
+                if(temp.left!=null){q.offer(temp.left);}
+                if(temp.right!=null){q.offer(temp.right);}
+            }
+        }
+        return depth;
     }
 }
