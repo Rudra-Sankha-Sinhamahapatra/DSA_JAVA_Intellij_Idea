e272a40f231dc869216ed07899d45b16a6e7f2d6
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-08-14 15:08:54 +0530

    Leetcode 543 added

--- src/com/rudra/DSA/Binary_Trees/Medium/Interview_Questions.java
+++ src/com/rudra/DSA/Binary_Trees/Medium/Interview_Questions.java
@@ -91,4 +91,22 @@ public class Interview_Questions {
         }
         return Math.max(leftHeight, rightHeight) + 1;
     }
+
+
+    //https://leetcode.com/problems/diameter-of-binary-tree/description/
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        int [] diameter = new int[1];
+        height(root,diameter);
+        return diameter[0];
+    }
+
+    private int height(TreeNode root,int[] diameter){
+        if(root == null) return 0;
+        int lh = height(root.left,diameter);
+        int rh = height(root.right,diameter);
+        diameter[0] = Math.max(diameter[0],lh+rh);
+        return 1 + Math.max(lh,rh);
+    }
+
 }
