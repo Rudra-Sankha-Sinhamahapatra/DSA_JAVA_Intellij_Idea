0fa75cf9bcc3a4b1d84eeaef544880e05dc7f143
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-08-16 16:51:14 +0530

    Leetcode 124 Added

--- src/com/rudra/DSA/Binary_Trees/Medium/Interview_Questions.java
+++ src/com/rudra/DSA/Binary_Trees/Medium/Interview_Questions.java
@@ -109,4 +109,21 @@ public class Interview_Questions {
         return 1 + Math.max(lh,rh);
     }
 
+//https://leetcode.com/problems/binary-tree-maximum-path-sum/
+    public int maxPathSum(TreeNode root) {
+        int maxValue [] = new int[1];
+        maxValue[0] = Integer.MIN_VALUE;
+        maxPathDown(root,maxValue);
+        return maxValue[0];
+    }
+
+    private int maxPathDown(TreeNode node,int maxValue[]){
+        if(node==null) return 0;
+        int left = Math.max(0,maxPathDown(node.left,maxValue));
+        int right = Math.max(0,maxPathDown(node.right,maxValue));
+        maxValue[0] =Math.max(maxValue[0],left+right+node.val);
+        return Math.max(left,right) + node.val;
+    }
+
+
 }
