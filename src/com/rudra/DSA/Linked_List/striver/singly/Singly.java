4ec3025b0962ee48d82fd8b434a4c7ae717fba93
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-12-05 13:02:56 +0530

    deletion of first,last,index added

--- src/com/rudra/DSA/Linked_List/striver/singly/Singly.java
+++ src/com/rudra/DSA/Linked_List/striver/singly/Singly.java
@@ -57,6 +57,80 @@ public class Singly{
         return head;
     }
 
+    Node deleteFirst (Node head) {
+        if(head == null) {
+            System.out.println("Linked List is empty nothig to delete");
+            return null;
+        }
+        Node temp = head;
+        head = head.next;
+        System.out.println("Deleted "+temp.data);
+        return head;
+    }
+
+    Node deleteLast (Node head) {
+        if(head==null){
+          return null;
+        }
+
+        else if(head.next==null){
+            return null;
+        }
+
+        Node temp = head;
+        while(temp.next.next!=null){
+            temp = temp.next;
+        }
+        System.out.println("Deleted " + temp.next.data);
+        temp.next = null;
+        return head;
+    }
+
+    int size (Node head) {
+        if(head == null) {
+            System.out.println("null");
+            return 0;
+
+        }
+        int size = 0;
+        Node temp = head;
+        while(temp!=null){
+            size++;
+            temp = temp.next;
+        }
+        return size;
+    }
+
+    Node get (Node head,int index){
+        Node node = head;
+      for(int i = 0 ;i<index;i++){
+          node = node.next;
+      }
+        return node;
+    }
+
+    Node deleteIndex (Node head,int index) {
+     if(head == null) {
+         return null;
+     }
+        int size = size(head);
+     if(index<0 || index>=size){
+         System.out.println("Invalid index "+index);
+         return head;
+     }
+     if(index == 0) {
+         return deleteFirst(head);
+     }
+    else if(index == size -1 ){
+        return deleteLast(head);
+     }
+     Node prev = get(head,index - 1);
+     int val = prev.next.data;
+     prev.next = prev.next.next;
+        System.out.println("Deleted " + val + " from index " + index);
+     return head;
+    }
+
     // Display linked list for testing
     public void printList(Node head) {
         Node current = head;
@@ -79,6 +153,12 @@ public class Singly{
         head = sol.insertAtEnd(head,34);
         head = sol.insertAtPosition(head,3,67);
         // Print the linked list
-        sol.printList(head);  // Output: 1 -> 2 -> 3 -> null
+        sol.printList(head);  // Output: 3 -> 2 -> 1 -> 67 -> 34 -> null
+        head = sol.deleteFirst(head);
+        sol.printList(head); // Output: 2 -> 1 -> 67 -> 34 -> null
+        head = sol.deleteLast(head);
+        sol.printList(head);// Output: 2 -> 1 -> 67 -> null
+        head = sol.deleteIndex(head,0);
+        sol.printList(head);//Output: 1 -> 67 -> null
     }
 }
